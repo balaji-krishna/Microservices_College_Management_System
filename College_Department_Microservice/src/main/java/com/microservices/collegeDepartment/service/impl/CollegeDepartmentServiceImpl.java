4765873aa0ed1c966177a297ad8026d30b11147b
Administrator <Administrator@DESKTOP-27TFO05>
2024-01-26 20:28:12 +0530

    The code added for resilience4j circuit breaker and retry mechanisms

--- College_Department_Microservice/src/main/java/com/microservices/collegeDepartment/service/impl/CollegeDepartmentServiceImpl.java
+++ College_Department_Microservice/src/main/java/com/microservices/collegeDepartment/service/impl/CollegeDepartmentServiceImpl.java
@@ -3,6 +3,8 @@ package com.microservices.collegeDepartment.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,11 +15,20 @@ import com.microservices.collegeDepartment.model.Student;
 import com.microservices.collegeDepartment.repository.CollegeDepartmentRepository;
 import com.microservices.collegeDepartment.service.CollegeDepartmentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @Service
 public class CollegeDepartmentServiceImpl implements CollegeDepartmentService {
 
 	@Autowired
 	private CollegeDepartmentRepository collegeDepartmentRepository;
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(CollegeDepartmentServiceImpl.class);
+	
+	public static final String COLLEGE_DEPT_SERVICE = "collegeDeptService";
+	
+	public static final String STUDENT_MICROSERVICE_BASE_URL = "http://STUDENT-MICROSERVICE/student/";
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -28,20 +39,33 @@ public class CollegeDepartmentServiceImpl implements CollegeDepartmentService {
 	}
 
 	@Override
+	@CircuitBreaker(name = COLLEGE_DEPT_SERVICE, fallbackMethod = "getDepartmentByIdFallback")
+	@Retry(name = COLLEGE_DEPT_SERVICE, fallbackMethod = "getDepartmentByIdFallback")
 	public ResponseBodyEntity getDepartmentById(int id) {
 		ResponseBodyEntity responseBodyEntity = new ResponseBodyEntity();
 		List<Student> students = new ArrayList<Student>();
 		CollegeDepartment collegeDepartment = collegeDepartmentRepository.findById(id).get();
 
-		students = restTemplate.getForObject("http://STUDENT-MICROSERVICE/student/getStudentByDepartmentId/" + id,
+		LOGGER.info("THE STUDENT MICROSERVICE IS CALLED");
+		students = restTemplate.getForObject(STUDENT_MICROSERVICE_BASE_URL + "getStudentByDepartmentId/" + id,
 				List.class);
 
 		responseBodyEntity.setCollegeDepartment(collegeDepartment);
 		responseBodyEntity.setStudents(students);
 		return responseBodyEntity;
 	}
+	
+	public ResponseBodyEntity getDepartmentByIdFallback(int id, Exception exception) {
+		LOGGER.info("THE STUDENT MICROSERVICE IS DOWN");
+		ResponseBodyEntity responseBodyEntity = new ResponseBodyEntity();
+		CollegeDepartment collegeDepartment = collegeDepartmentRepository.findById(id).get();
+
+		responseBodyEntity.setCollegeDepartment(collegeDepartment);
+		return responseBodyEntity;
+	}
 
 	@Override
+	@Retry(name = COLLEGE_DEPT_SERVICE, fallbackMethod = "getAllDepartmentsInfoFallback")
 	public List<ResponseBodyEntity> getAllDepartmentsInfo() {
 		List<ResponseBodyEntity> responseBodyEntities = new ArrayList<ResponseBodyEntity>();
 		List<CollegeDepartment> collegeDepartments = collegeDepartmentRepository.findAll();
@@ -49,9 +73,24 @@ public class CollegeDepartmentServiceImpl implements CollegeDepartmentService {
 		for (CollegeDepartment collegeDepartment : collegeDepartments) {	
 			List<Student> students = new ArrayList<Student>();
 			ResponseBodyEntity responseBodyEntity = new ResponseBodyEntity();
-			students = restTemplate.getForObject("http://STUDENT-MICROSERVICE/student/getStudentByDepartmentId/"
+			LOGGER.info("THE STUDENT MICROSERVICE IS CALLED");
+			students = restTemplate.getForObject(STUDENT_MICROSERVICE_BASE_URL + "getStudentByDepartmentId/"
 					+ collegeDepartment.getDepartmentId(), List.class);
 			responseBodyEntity.setStudents(students);
+			responseBodyEntity.setCollegeDepartment(collegeDepartment);
+			responseBodyEntities.add(responseBodyEntity);
+		}		
+		
+		return responseBodyEntities;
+	}
+	
+	public List<ResponseBodyEntity> getAllDepartmentsInfoFallback(Exception exception) {
+		LOGGER.info("THE STUDENT MICROSERVICE IS DOWN");	
+		List<ResponseBodyEntity> responseBodyEntities = new ArrayList<ResponseBodyEntity>();
+		List<CollegeDepartment> collegeDepartments = collegeDepartmentRepository.findAll();
+
+		for (CollegeDepartment collegeDepartment : collegeDepartments) {
+			ResponseBodyEntity responseBodyEntity = new ResponseBodyEntity();
 			responseBodyEntity.setCollegeDepartment(collegeDepartment);
 			responseBodyEntities.add(responseBodyEntity);
 		}		
